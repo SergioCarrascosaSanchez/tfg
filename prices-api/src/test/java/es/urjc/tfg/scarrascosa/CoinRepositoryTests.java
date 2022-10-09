6452d9633bd58b90332e53acf5a5d554f5f8acec
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-10-10 00:28:22 +0200

    CoinRepository tests add lists to save test case

--- prices-api/src/test/java/es/urjc/tfg/scarrascosa/CoinRepositoryTests.java
+++ prices-api/src/test/java/es/urjc/tfg/scarrascosa/CoinRepositoryTests.java
@@ -2,6 +2,7 @@ package es.urjc.tfg.scarrascosa;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,20 @@ class CoinRepositoryTests {
     @Test
     void testSave() {
         
+        double price1 = 10.0;
+        double price2 = 30.0;
+        double price3 = 40.0;
+        double price4 = 70.0;
+        double price5 = 10.0;
+        double price6 = 90.0;
+        
         Coin coin = new Coin("SCSBUSD");
+        coin.addLastPrice(price1);
+        coin.addLastPrice(price2);
+        coin.add30mPrice(price3);
+        coin.add30mPrice(price4);
+        coin.addToListOfAllPrices(price5);
+        coin.addToListOfAllPrices(price6);
         
         Coin savedCoin = this.coinRepository.save(coin);
         
@@ -28,6 +42,12 @@ class CoinRepositoryTests {
         assertThat(optionalCoin.isPresent()).isEqualTo(true);
         
         assertThat(optionalCoin.get().getId()).isEqualTo(savedCoin.getId());
+        
+        assertThat(optionalCoin.get().getLastPrice()).isEqualTo(price2);
+        assertThat(Arrays.asList(optionalCoin.get().getListOf10sPrices().toArray())).isEqualTo(Arrays.asList(price1, price2));
+        assertThat(Arrays.asList(optionalCoin.get().getListOf30mPrices().toArray())).isEqualTo(Arrays.asList(price3, price4));
+        assertThat(Arrays.asList(optionalCoin.get().getListOfAllPrices().toArray())).isEqualTo(Arrays.asList(price5, price6));
+        
     }
     
     @Test
