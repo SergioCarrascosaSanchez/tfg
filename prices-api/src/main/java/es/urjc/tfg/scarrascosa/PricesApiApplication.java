1c39a01faf85c156b7cfb6346a25b5716873310a
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-10-11 22:41:17 +0200

    schedule tasks
    create BinanceResponseDTO + tests + schedule task for 1d, 30m and 10s + add POM dependencies and application.propierties H2 console

--- prices-api/src/main/java/es/urjc/tfg/scarrascosa/PricesApiApplication.java
+++ prices-api/src/main/java/es/urjc/tfg/scarrascosa/PricesApiApplication.java
@@ -2,8 +2,10 @@ package es.urjc.tfg.scarrascosa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PricesApiApplication {
 
 	public static void main(String[] args) {
---
+++ prices-api/src/main/java/es/urjc/tfg/scarrascosa/ScheduledTasks/ScheduledTasks.java
@@ -0,0 +1,99 @@
+package es.urjc.tfg.scarrascosa.ScheduledTasks;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import es.urjc.tfg.scarrascosa.Coin.Coin;
+import es.urjc.tfg.scarrascosa.Coin.CoinRepository;
+import es.urjc.tfg.scarrascosa.DTO.BinanceResponseDTO;
+
+@Component
+public class ScheduledTasks {
+    
+    @Autowired
+    private CoinRepository coinRepo;
+    
+    private RestTemplate restTemplate = new RestTemplate();
+    
+    public List<BinanceResponseDTO> fetchApiCall(){
+        ResponseEntity<BinanceResponseDTO[]> response = restTemplate.exchange("https://api.binance.com/api/v3/ticker/price", HttpMethod.GET, null, BinanceResponseDTO[].class);
+        return Arrays.stream(response.getBody()).filter(coin -> coin.getSymbol().endsWith("BUSD")).toList();
+    }
+    
+    @Scheduled(fixedRate = 10000, initialDelay = 10000)
+    public void fetchLastValues() {
+        
+        List<BinanceResponseDTO> responseFilteredList = fetchApiCall();
+        
+        for(BinanceResponseDTO coinFromFilteredList : responseFilteredList) {
+            String symbol = coinFromFilteredList.getSymbol();
+            Double value = coinFromFilteredList.getPrice();
+            
+            Optional<Coin> optionalCoin = this.coinRepo.findByTicker(symbol);
+            if(optionalCoin.isPresent()) {
+                Coin coin = optionalCoin.get();
+                coin.addLastPrice(value);
+                this.coinRepo.save(coin);
+            }
+            else {
+                Coin coin = new Coin(symbol);
+                coin.addLastPrice(value);
+                this.coinRepo.save(coin);
+            }
+        }
+    }
+    
+    @Scheduled(fixedRate = 1800000 ,initialDelay = 15000)
+    public void fetch30mValues() {
+        
+        List<BinanceResponseDTO> responseFilteredList = fetchApiCall();
+        
+        for(BinanceResponseDTO coinFromFilteredList : responseFilteredList) {
+            String symbol = coinFromFilteredList.getSymbol();
+            Double value = coinFromFilteredList.getPrice();
+            
+            Optional<Coin> optionalCoin = this.coinRepo.findByTicker(symbol);
+            if(optionalCoin.isPresent()) {
+                Coin coin = optionalCoin.get();
+                coin.add30mPrice(value);
+                this.coinRepo.save(coin);
+            }
+            else {
+                Coin coin = new Coin(symbol);
+                coin.add30mPrice(value);
+                this.coinRepo.save(coin);
+            }
+        }
+    }
+    
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void fetchAllValues() {
+        
+        List<BinanceResponseDTO> responseFilteredList = fetchApiCall();
+        
+        for(BinanceResponseDTO coinFromFilteredList : responseFilteredList) {
+            String symbol = coinFromFilteredList.getSymbol();
+            Double value = coinFromFilteredList.getPrice();
+            
+            Optional<Coin> optionalCoin = this.coinRepo.findByTicker(symbol);
+            if(optionalCoin.isPresent()) {
+                Coin coin = optionalCoin.get();
+                coin.addToListOfAllPrices(value);
+                this.coinRepo.save(coin);
+            }
+            else {
+                Coin coin = new Coin(symbol);
+                coin.addToListOfAllPrices(value);
+                this.coinRepo.save(coin);
+            }
+        }
+    }
+}
