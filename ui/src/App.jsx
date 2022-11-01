e2de60c5f495b61b4abe20a07dd31a64565e0923
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-11-01 13:15:15 +0100

    add CssVarsProvider for mui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,9 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
+import { CssVarsProvider } from '@mui/joy/styles';
 function App() {
   return (
-    <Routes>
-      <Route path="/"></Route>
-    </Routes>
+    <CssVarsProvider>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </CssVarsProvider>
   )
 }
 
