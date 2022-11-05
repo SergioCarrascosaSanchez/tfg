e2281b7ad6aac3963cf3b9e357c65c86c7724e7c
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-11-05 15:28:28 +0100

    mainPage first version

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,10 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
-import { CssVarsProvider } from '@mui/joy/styles';
+import { CssVarsProvider} from '@mui/joy/styles';
+import { MainPage } from './pages/MainPage/MainPage';
+import { customTheme } from './config/customTheme'
+
 function App() {
   return (
-    <CssVarsProvider>
+    <CssVarsProvider theme={customTheme}>
       <Routes>
-        <Route path="/"></Route>
+        <Route path="/" element={<MainPage/>}></Route>
       </Routes>
     </CssVarsProvider>
   )
