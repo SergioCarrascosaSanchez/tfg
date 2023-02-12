34769fae2cfe05016688b52a4f9bdcb9b8c6f323
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-02-12 19:10:09 +0100

    student page with data. No quantity

--- ui/src/pages/StudentPage/StudentPage.jsx
+++ ui/src/pages/StudentPage/StudentPage.jsx
@@ -1,10 +1,14 @@
 import { Navbar } from "../../components/Navbar/Navbar";
-import { useParams } from "react-router-dom";
-import { CircularProgress } from "@mui/joy";
+import { useParams, Link } from "react-router-dom";
+import { CircularProgress, Box } from "@mui/joy";
 import { useGetUserData } from "../../hooks/useGetUserData";
 import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
+import { CoinChartCard } from "../../components/CoinChartCard/CoinChartCard";
 
-export const StudentPageError = "Ha ocurrido un error obteniendo la información del estudiante"
+export const StudentPageError =
+  "Ha ocurrido un error obteniendo la información del estudiante";
+
+export const StudentInvestmentsTitle = "Mis inversiones";
 
 export const StudentPage = () => {
   const student = useParams().student;
@@ -12,13 +16,26 @@ export const StudentPage = () => {
   return (
     <>
       <Navbar />
-      <h1>{student}</h1>
       {userData.loading ? (
-        <CircularProgress />
+        <>
+          <h1>{student}</h1>
+          <CircularProgress />
+        </>
       ) : userData.error ? (
-        <ErrorMessage message={StudentPageError} center={true}/>
+        <>
+          <h1>{student}</h1>
+          <ErrorMessage message={StudentPageError} center={true} />
+        </>
       ) : (
-        <></>
+        <>
+          <h1>{student}</h1>
+          <h2>{`Balance: ${userData.data.balance}$`}</h2>
+          <h2>{StudentInvestmentsTitle}</h2>
+          {userData.data.coins.map((investment) => {
+            return <CoinChartCard name={investment.coin} time={"30m"} />;
+          })}
+          {/*Tengo que hacer un componente genérico que se llame CoinChartCardList*/}
+        </>
       )}
     </>
   );
