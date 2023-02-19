6266b16d8500e259d754cf74ea5c7620d18559d1
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-02-19 17:49:21 +0100

    useGetUserData hook implementation

--- ui/src/hooks/useGetUserData.js
+++ ui/src/hooks/useGetUserData.js
@@ -1,1 +1,36 @@
-export const useGetUserData = () =>{}
+import { useEffect, useState } from "react";
+
+const URL = `${import.meta.env.VITE_USERS_API_URL}`;
+
+export function useGetUserData(username) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [statusCode, setStatusCode] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${URL}/users/${username}`, {
+      method: "GET",
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          res.json().then((resData) => {
+            setStatusCode(res.status);
+            setData(resData);
+            setLoading(false);
+          });
+        } else {
+          setStatusCode(res.status);
+          setLoading(false);
+          setError(true);
+        }
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
+        console.log(err)
+      });
+  }, []);
+
+  return { loading, error, statusCode, data };
+}
