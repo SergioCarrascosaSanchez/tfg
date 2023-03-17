e1f75cf0e64c89379096491bf08b1a36119c006e
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-03-17 11:27:52 +0100

    create UserPage + loading and error

---
+++ ui/src/pages/UserPage/UserPage.jsx
@@ -0,0 +1,50 @@
+import { Navbar } from "../../components/Navbar/Navbar";
+import { useParams } from "react-router-dom";
+import { CircularProgress, Box, Typography } from "@mui/joy";
+import { useGetUserData } from "../../hooks/useGetUserData";
+import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
+
+export const UserPageError =
+  "Ha ocurrido un error obteniendo la información del usuario";
+
+export const UnexpectedUserPageError = "Ha ocurrido un error inesperado";
+
+export const UserPage = () => {
+  const user = useParams().user;
+  const userData = useGetUserData(user);
+  return (
+    <>
+      <Navbar />
+      <Box sx={{ mx: "20vw", my: "5vh", maxWidth: "1000px" }}>
+        {userData.loading ? (
+          <>
+            <Typography level="display2" component="h1">
+              {user}
+            </Typography>
+            <Box sx={{ display: "grid", placeContent: "center" }}>
+              <CircularProgress />
+            </Box>
+          </>
+        ) : userData.error ? (
+          <>
+            <Typography level="display2" component="h1">
+              {user}
+            </Typography>
+            <ErrorMessage message={UserPageError} center={true} />
+          </>
+        ) : userData.data.role === "STUDENT" ? (
+          <></>
+        ) : userData.data.role === "TEACHER" ? (
+          <></>
+        ) : (
+          <>
+            <Typography level="display2" component="h1">
+              {user}
+            </Typography>
+            <ErrorMessage message={UnexpectedUserPageError} center={true} />
+          </>
+        )}
+      </Box>
+    </>
+  );
+};
---
+++ ui/tests/UserPage/UnexpectedError_UserPage.test.jsx
@@ -0,0 +1,60 @@
+import { describe } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import { options, appName } from "../../src/components/Navbar/Navbar";
+import { UserPage, UnexpectedUserPageError } from "../../src/pages/UserPage/UserPage";
+
+const username = "Sergio";
+
+describe("UserPage", () => {
+    vi.mock("react-router-dom", () => {
+        const useParams = vi.fn();
+        const username = "Sergio";
+        useParams.mockReturnValue({
+          user: username,
+        });
+        return {
+          useParams,
+        };
+      });
+    
+      vi.mock("../../src/hooks/useGetUserData", () => {
+        const useGetUserData = vi.fn();
+        useGetUserData.mockReturnValue({
+          loading: false,
+          error: false,
+          statusCode: 200,
+          data: {
+            username:"Sergio",
+            role:"OTHER_ROLE",
+            balance: 1000.0,
+            portfolio: [
+              {
+                coin: "BTC",
+                quantity: 10,
+              },
+              {
+                coin: "ETH",
+                quantity: 2,
+              },
+            ],
+          },
+        });
+        return {
+          useGetUserData,
+        };
+      });
+    
+      afterEach(cleanup);
+    
+      it("should render navbar", () => {
+        render(<UserPage />);
+        options.forEach((option) => screen.getByText(option));
+        screen.getByText(appName);
+      });
+    
+      it("should render error", () => {
+        render(<UserPage />);
+        screen.getByText(username);
+        screen.getByText(UnexpectedUserPageError);
+      });
+})
