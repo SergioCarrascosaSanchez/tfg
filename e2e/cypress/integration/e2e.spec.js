54960122e75466939a390b0f1514dfdfad619b12
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-03-21 23:09:52 +0100

    e2e testing for teacher page

--- e2e/cypress/integration/e2e.spec.js
+++ e2e/cypress/integration/e2e.spec.js
@@ -6,21 +6,21 @@ describe("Complete app", () => {
   describe("BuyCoin", () => {
     it("Buy coin", () => {
       cy.request({
-        method: 'POST',
-        url: 'http://users-api:8081/signup',
+        method: "POST",
+        url: "http://users-api:8081/signup",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: {
-          "username":"UserTest",
-          "email":"UserTest@UserTest.com",
-          "password":"elrijjkfh",
-          "initialBalance":1000,
-          "roles": ["STUDENT"]
-      },
+          username: "UserTest",
+          email: "UserTest@UserTest.com",
+          password: "elrijjkfh",
+          initialBalance: 1000,
+          roles: ["STUDENT"],
+        },
       })
-        .its('status')
-        .should('equal', 200)
+        .its("status")
+        .should("equal", 200);
 
       cy.visit("/");
       cy.contains("Iniciar sesión").click();
@@ -97,7 +97,11 @@ describe("Complete app", () => {
       cy.get("@newBalance").then((balance) => {
         cy.get("@priceFirstSell").then((price) => {
           const expectedNewBalance = balance + price;
-          cy.get("@newBalanceAfterFirstSell").should("be.closeTo", expectedNewBalance, 0.01);
+          cy.get("@newBalanceAfterFirstSell").should(
+            "be.closeTo",
+            expectedNewBalance,
+            0.01
+          );
         });
       });
       cy.visit("/market");
@@ -117,7 +121,7 @@ describe("Complete app", () => {
 
       cy.visit("/users/UserTest");
       cy.contains("UserTest");
-      cy.contains('ADA').should('not.exist');
+      cy.contains("ADA").should("not.exist");
       cy.contains("Balance:")
         .invoke("text")
         .then((text) => {
@@ -127,7 +131,11 @@ describe("Complete app", () => {
       cy.get("@newBalanceAfterFirstSell").then((balance) => {
         cy.get("@priceSecondSell").then((price) => {
           const expectedNewBalance = balance + price;
-          cy.get("@newBalanceAfterSecondSell").should("be.closeTo", expectedNewBalance, 0.01);
+          cy.get("@newBalanceAfterSecondSell").should(
+            "be.closeTo",
+            expectedNewBalance,
+            0.01
+          );
         });
       });
     });
@@ -156,6 +164,118 @@ describe("Complete app", () => {
       cy.get("[placeholder='Buscar criptomoneda']").type("BTCBUSD");
       cy.contains("Buscar").click();
       cy.url().should("include", "/coins/BTC");
+    });
+  });
+  describe("Teacher and student trades", () => {
+    it("show show student trades", () => {
+      cy.request({
+        method: "POST",
+        url: "http://users-api:8081/signup",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          username: "StudentForE2E",
+          email: "StudentForE2E@UserTest.com",
+          password: "elrijjkfh",
+          initialBalance: 1000,
+          roles: ["STUDENT"],
+        },
+      })
+        .its("status")
+        .should("equal", 200);
+
+      cy.request({
+        method: "POST",
+        url: "http://users-api:8081/signup",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          username: "TeacherForE2E",
+          email: "TeacherForE2E@UserTest.com",
+          password: "elrijjkfh2",
+          initialBalance: 1000,
+          roles: ["TEACHER"],
+        },
+      })
+        .its("status")
+        .should("equal", 200);
+
+      cy.request({
+        method: "POST",
+        url: "http://users-api:8081/teacher/TeacherForE2E",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          studentList: ["StudentForE2E"],
+        },
+      })
+        .its("status")
+        .should("equal", 200);
+
+      cy.visit("/");
+      cy.contains("Iniciar sesión").click();
+      cy.get('[name="username"]').type("StudentForE2E");
+      cy.get('[name="password"]').type("elrijjkfh");
+      cy.get('[data-testid="submitLoginButton"]').click();
+      cy.contains("StudentForE2E");
+      cy.visit("/market");
+      cy.contains("ADA").click();
+      cy.contains("ADA");
+      cy.get('[data-testid="ADAPrice"]')
+        .invoke("text")
+        .then((text) => {
+          const price = text.slice(0, -2);
+          cy.wrap(Number(price)).as("price");
+        });
+      cy.contains("Transacción");
+      cy.get('[placeholder="Cantidad"]').type("2");
+      cy.get('[placeholder="Justificacion"]').type("texto de prueba");
+      cy.contains("Comprar").click();
+      cy.contains("Transacción realizada con éxito!");
+
+      cy.visit("/market");
+      cy.contains("ADA").click();
+      cy.contains("ADA");
+      cy.get('[data-testid="ADAPrice"]')
+        .invoke("text")
+        .then((text) => {
+          const price = text.slice(0, -2);
+          cy.wrap(Number(price)).as("price2");
+        });
+      cy.contains("Transacción");
+      cy.get('[placeholder="Cantidad"]').type("1");
+      cy.get('[placeholder="Justificacion"]').type("texto de prueba");
+      cy.contains("Vender").click();
+      cy.contains("Transacción realizada con éxito!");
+
+      cy.visit("/");
+      cy.contains("Iniciar sesión").click();
+      cy.get('[name="username"]').type("TeacherForE2E");
+      cy.get('[name="password"]').type("elrijjkfh2");
+      cy.get('[data-testid="submitLoginButton"]').click();
+
+      cy.visit("/users/TeacherForE2E");
+      cy.contains("Transacciones: 2");
+      cy.contains("StudentForE2E").click();
+      cy.contains("Transacciones de StudentForE2E");
+
+      cy.get('[data-testid="StudentForE2ETradeChart0"]');
+      cy.get('[data-testid="StudentForE2ETrade0CoinADA"]');
+      cy.contains("Compra");
+      cy.contains("Cantidad: 2");
+      cy.get('[data-testid="StudentForE2ETradeChart1"]');
+      cy.get('[data-testid="StudentForE2ETrade1CoinADA"]');
+      cy.contains("Venta");
+      cy.contains("Cantidad: 1");
+      cy.get("@price").then((priceBuy) => {
+        cy.contains(`Precio: ${priceBuy}`);
+      });
+      cy.get("@price2").then((priceSell) => {
+        cy.contains(`Precio: ${priceSell}`);
+      });
     });
   });
 });
