97e2c78f1a930deaaa77c7dbe18c33562a00f263
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-04-07 14:48:33 +0200

    make TradeCard responsive

--- ui/src/components/StudentTradingInfo/StudentTradingInfo.jsx
+++ ui/src/components/StudentTradingInfo/StudentTradingInfo.jsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Card, List } from "@mui/joy";
+import { Typography, List } from "@mui/joy";
 import { TradeCard } from "../TradeCard/TradeCard";
 
 export const NoTradesMessage = "No hay transacciones";
@@ -8,21 +8,44 @@ export const StudentTradingInfo = ({ info, username }) => {
     <>
       {info.length > 0 ? (
         <>
-        <Typography level="h2" component="h2" sx={{px:3, paddingBottom:"15px", paddingTop:"20px"}}>
-          {`Transacciones de ${username}`}
-        </Typography>
-        <List
-          sx={{ overflowY: "scroll", overflowX: "hidden", maxHeight: "80vh" }}
-        >
-          {info.map((trade, index) => (
-            <TradeCard
-              key={`${username}TradeCard${index}`}
-              trade={trade}
-              username={username}
-              index={index}
-            />
-          ))}
-        </List>
+          <Typography
+            component="h2"
+            sx={{
+              paddingBottom: {
+                xs: "10px",
+                md: "10px",
+                lg: "15px",
+                xl: "15px",
+              },
+              paddingTop: "20px",
+              typography: {
+                xs: "display4",
+                md: "h2",
+                lg: "h2",
+                xl: "h2",
+              },
+              mx: {
+                xs: 0,
+                md: 3,
+                lg: 3,
+                xl: 3,
+              },
+            }}
+          >
+            {`Transacciones de ${username}`}
+          </Typography>
+          <List
+            sx={{ overflowY: "scroll", overflowX: "hidden", maxHeight: "80vh" }}
+          >
+            {info.map((trade, index) => (
+              <TradeCard
+                key={`${username}TradeCard${index}`}
+                trade={trade}
+                username={username}
+                index={index}
+              />
+            ))}
+          </List>
         </>
       ) : (
         <Typography level="p" component="p" textColor="neutral.500">
