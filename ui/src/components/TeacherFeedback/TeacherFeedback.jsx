c845018705de512de0f94e20ec15e9b8d1e06b64
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-04-15 15:43:48 +0200

    responsive TeacherFeedback component

--- ui/src/components/TeacherFeedback/TeacherFeedback.jsx
+++ ui/src/components/TeacherFeedback/TeacherFeedback.jsx
@@ -1,14 +1,14 @@
-import { Button, Textarea } from "@mui/joy";
+import { Box, Button, Textarea } from "@mui/joy";
 import { useState } from "react";
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { useUpdateComment } from "../../hooks/useUpdateComment";
 import { useContext } from "react";
-import { UserContext } from "../../context/UserContext"
+import { UserContext } from "../../context/UserContext";
 
 export const TeacherFeedbackErrors = {
   NoComment: "No hay ningún comentario de profesor aún",
   EmptyTextArea: "No puedes dejar esta sección vacía",
-  UnexpectedError: "Ha ocurrido un error"
+  UnexpectedError: "Ha ocurrido un error",
 };
 
 export const TeacherFeedbackElements = {
@@ -19,18 +19,27 @@ export const TeacherFeedback = ({ comment, role, student, tradeId }) => {
   const [newComment, setNewComment] = useState("");
   const [emptyCommentError, setEmptyCommentError] = useState(false);
   const { loading, error, statusCode, UpdateComment } = useUpdateComment();
-  
-  const contextLoading = useContext(UserContext).loading
+
+  const contextLoading = useContext(UserContext).loading;
 
   const handleSubmit = () => {
     setEmptyCommentError(newComment.length === 0);
     if (newComment.length > 0) {
-      UpdateComment(window.localStorage.getItem("username"), student, tradeId, newComment)
+      UpdateComment(
+        window.localStorage.getItem("username"),
+        student,
+        tradeId,
+        newComment
+      );
     }
   };
 
   return (
-    <>
+    <Box
+      sx={{
+        maxWidth: {xs:"100%", md:"80%", lg:"80%", xl:"80%"},
+      }}
+    >
       {comment.length > 0 ? (
         <p>{comment}</p>
       ) : role === "TEACHER" ? (
@@ -47,14 +56,19 @@ export const TeacherFeedback = ({ comment, role, student, tradeId }) => {
               form={true}
             />
           )}
-          <Textarea onChange={(e) => setNewComment(e.target.value)} />
-          <Button loading={(loading || contextLoading)} onClick={handleSubmit}>
-            {TeacherFeedbackElements.SubmitButton}
-          </Button>
+          <Textarea
+            minRows={2}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <Box size={"xs"} sx={{display:"grid", placeContent:"right", my:1}}>
+            <Button loading={loading || contextLoading} onClick={handleSubmit}>
+              {TeacherFeedbackElements.SubmitButton}
+            </Button>
+          </Box>
         </>
       ) : (
         role === "STUDENT" && <p>{TeacherFeedbackErrors.NoComment}</p>
       )}
-    </>
+    </Box>
   );
 };
