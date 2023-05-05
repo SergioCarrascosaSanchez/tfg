37c6f3e74cb9366e8992b3d285464ed096891c5d
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-05-05 10:53:04 +0200

    change useAuth implementation

--- ui/src/hooks/useAuth.js
+++ ui/src/hooks/useAuth.js
@@ -1,17 +1,6 @@
-import { useState, useEffect } from "react";
-
-export const useAuth = ()  => {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
-
-  useEffect(() => {
-    const username = localStorage.getItem("username");
-    const token = localStorage.getItem("token");
-    if (username !== null && token !== null) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-  }, []);
-
-  return isAuthenticated;
-}
+export const useAuth = () => {
+  return (
+    localStorage.getItem("username") !== null &&
+    localStorage.getItem("token") !== null
+  );
+};
