b17fb347199f95cc7e510c60f5642a6e43e796cd
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2023-05-18 17:06:52 +0200

    most DTO Unit Tests

---
+++ users-api/src/test/java/es/urjc/tfg/scarrascosa/unitTesting/FeedbackDTOUnitTests.java
@@ -0,0 +1,22 @@
+package es.urjc.tfg.scarrascosa.unitTesting;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import es.urjc.tfg.scarrascosa.DTO.FeedbackDTO;
+
+public class FeedbackDTOUnitTests {
+    @Test
+    public void testGetFeedback() {
+        String feedback = "This is a feedback";
+        FeedbackDTO feedbackDTO = new FeedbackDTO(feedback);
+        Assertions.assertEquals(feedback, feedbackDTO.getFeedback());
+    }
+
+    @Test
+    public void testSetFeedback() {
+        String feedback = "This is a feedback";
+        FeedbackDTO feedbackDTO = new FeedbackDTO();
+        feedbackDTO.setFeedback(feedback);
+        Assertions.assertEquals(feedback, feedbackDTO.getFeedback());
+    }
+}
---
+++ users-api/src/test/java/es/urjc/tfg/scarrascosa/unitTesting/LoginFormUserDTOUnitTests.java
@@ -0,0 +1,38 @@
+package es.urjc.tfg.scarrascosa.unitTesting;
+
+import es.urjc.tfg.scarrascosa.DTO.LoginFormUserDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoginFormUserDTOUnitTests {
+    @Test
+    public void testGetUsername() {
+        String username = "name";
+        LoginFormUserDTO loginFormUserDTO = new LoginFormUserDTO(username, "password123");
+        Assertions.assertEquals(username, loginFormUserDTO.getUsername());
+    }
+
+    @Test
+    public void testSetUsername() {
+        String username = "name";
+        LoginFormUserDTO loginFormUserDTO = new LoginFormUserDTO();
+        loginFormUserDTO.setUsername(username);
+        Assertions.assertEquals(username, loginFormUserDTO.getUsername());
+    }
+
+    @Test
+    public void testGetPassword() {
+        String password = "password123";
+        LoginFormUserDTO loginFormUserDTO = new LoginFormUserDTO("name", password);
+        Assertions.assertEquals(password, loginFormUserDTO.getPassword());
+    }
+
+    @Test
+    public void testSetPassword() {
+        String password = "password123";
+        LoginFormUserDTO loginFormUserDTO = new LoginFormUserDTO();
+        loginFormUserDTO.setPassword(password);
+        Assertions.assertEquals(password, loginFormUserDTO.getPassword());
+    }
+
+}
